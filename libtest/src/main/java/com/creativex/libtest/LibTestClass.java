6772ada52f86abdf0e2a4683172218832c8de601
Kartik Srivastava <kartiksrivastava20590@gmail.com>
2018-07-09 17:21:54 +0530

    create project and libtest lib module

---
+++ libtest/src/main/java/com/creativex/libtest/LibTestClass.java
@@ -0,0 +1,16 @@
+package com.creativex.libtest;
+
+import android.util.Log;
+
+/**
+ * Created by kartiksrivastava on 09/07/18.
+ */
+
+public class LibTestClass {
+
+    private static final String TAG = "LibTestClass";
+
+    public static void logTestMethod(String message) {
+        Log.e("TAG", message);
+    }
+}
